59bac94a53710e2fb2d347f30fac7dc17a05111c
ashwin19971 <ashwingiri1997@gmail.com>
2020-08-01 23:35:16 +0530

    Updates

---
+++ precache-manifest.fe723ae6533e6c27c7960268db91ebe7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ff5db0ba91dc858ab4f9db6cd22870",
+    "url": "/ashwin19971/college-web.git/index.html"
+  },
+  {
+    "revision": "817dfdf19a1b46556c23",
+    "url": "/ashwin19971/college-web.git/static/css/main.b4a5faa8.chunk.css"
+  },
+  {
+    "revision": "72d1ca5a6513fb39c4b5",
+    "url": "/ashwin19971/college-web.git/static/js/2.b86c569c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ashwin19971/college-web.git/static/js/2.b86c569c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "817dfdf19a1b46556c23",
+    "url": "/ashwin19971/college-web.git/static/js/main.848cb31f.chunk.js"
+  },
+  {
+    "revision": "d540a6d753c6a1c9b2e7",
+    "url": "/ashwin19971/college-web.git/static/js/runtime-main.f5f7f764.js"
+  },
+  {
+    "revision": "11c2cd068a4bb8c39a638bb2f56eb0b1",
+    "url": "/ashwin19971/college-web.git/static/media/fontello.11c2cd06.woff2"
+  },
+  {
+    "revision": "9975fb69b9f2e33b1099b5968a6916a1",
+    "url": "/ashwin19971/college-web.git/static/media/fontello.9975fb69.eot"
+  },
+  {
+    "revision": "a108f659bcc06170b618c1be53213973",
+    "url": "/ashwin19971/college-web.git/static/media/fontello.a108f659.woff"
+  },
+  {
+    "revision": "e08f4fdc5cbdc1b98f29fd06e6afd7ba",
+    "url": "/ashwin19971/college-web.git/static/media/fontello.e08f4fdc.ttf"
+  },
+  {
+    "revision": "ef01032c1407dec1cbc19baa16617443",
+    "url": "/ashwin19971/college-web.git/static/media/fontello.ef01032c.svg"
+  }
+]);
